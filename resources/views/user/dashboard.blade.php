9dc600c2006dab435f02ba3cc7e8fd3f174dc1bb
nambelaas <salmaniseif@gmail.com>
2023-09-19 10:33:00 +0700

    update dashboard page user & admin

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -19,37 +19,41 @@
                     <tbody>
                         @forelse ($checkouts as $checkout)
                             <tr class="align-middle">
-                            <td width="18%">
-                                <img src="{{ asset('images/item_bootcamp.png') }}" height="120" alt="">
-                            </td>
-                            <td>
-                                <p class="mb-2">
-                                    <strong>{{ $checkout->Camp->title }}</strong>
-                                </p>
-                                <p>
-                                    {{ $checkout->created_at->format('M d, Y') }}
-                                </p>
-                            </td>
-                            <td>
-                                <strong>$280,000</strong>
-                            </td>
-                            <td>
-                                @if ($checkout->is_paid)
-                                    <strong class="text-success">Payment Success</strong>
-                                @else
-                                    <strong>Waiting for Payment</strong>
-                                @endif
-                            </td>
-                            <td>
-                                <a href="https://wa.me/+6285707492381?text=Hi, saya ingin bertanya tentang kelas {{ $checkout->Camp->title }}" class="btn btn-primary">
-                                    Contact Support
-                                </a>
-                            </td>
-                        </tr>
+                                <td width="18%">
+                                    <img src="{{ asset('images/item_bootcamp.png') }}" height="120" alt="">
+                                </td>
+                                <td>
+                                    <p class="mb-2">
+                                        <strong>{{ $checkout->Camp->title }}</strong>
+                                    </p>
+                                    <p>
+                                        {{ $checkout->created_at->format('M d, Y') }}
+                                    </p>
+                                </td>
+                                <td>
+                                    <strong>${{ $checkout->Camp->price }}</strong>
+                                </td>
+                                <td>
+                                    <strong>{{ $checkout->payment_status }}</strong>
+                                </td>
+                                <td>
+                                    @if ($checkout->payment_status === 'waiting')
+                                        <a href="{{ $checkout->midtrans_url }}" class="btn btn-primary">
+                                            Pay
+                                        </a>
+                                    @endif
+                                </td>
+                                <td>
+                                    <a href="https://wa.me/+6285707492381?text=Hi, saya ingin bertanya tentang kelas {{ $checkout->Camp->title }}"
+                                        class="btn btn-primary">
+                                        Contact Support
+                                    </a>
+                                </td>
+                            </tr>
                         @empty
                             <tr>
                                 <td colspan="5">
-                                    <h3>No data</h3>
+                                    <h3>No Camp Registered</h3>
                                 </td>
                             </tr>
                         @endforelse
